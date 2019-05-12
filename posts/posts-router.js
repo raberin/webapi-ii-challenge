5482e7b78a6ac9edb19e98980049dcb281730b47
raberin <rmvaberin@gmail.com>
2019-05-12 16:15:28 -0700

    Get by id and Post

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -16,4 +16,40 @@ router.get("/", async (req, res) => {
   }
 });
 
+router.post("/", async (req, res) => {
+  const newPost = req.body;
+  if (newPost.title && newPost.contents) {
+    try {
+      const post = await Db.insert(newPost);
+      res.status(201).json(post);
+    } catch (err) {
+      res.status(500).json({
+        err: "There was an error while saving the post to the database"
+      });
+    }
+  } else {
+    res.status(400).json({
+      errorMessage: "Please provide title and contents for the post."
+    });
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const specificPost = await Db.findById(id);
+    if (id) {
+      res.json(specificPost);
+    } else {
+      //Error doesnt seem to work
+      res.status(404).json("The post with the specified ID does not exist.");
+    }
+  } catch (err) {
+    res.status(500).json({
+      err: "The post information could not be retrieved."
+    });
+  }
+});
+
 module.exports = router;
